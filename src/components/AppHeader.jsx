da46ab3f1b078bdca8c2922d46b0e4f344556e82
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-07-29 11:43:10 +0200

    1st attempt to use API_KEY

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 export default function AppHeader() {
 
 
-    const ApiUrl = 'https://api.themoviedb.org/3/search/movie';
-    const [movies, setMovies] = useState('');
     const API_KEY = import.meta.env.VITE_API_KEY;
+    const ApiUrl = `https://api.themoviedb.org/3/search/movie/${API_KEY}`;
+    const [movies, setMovies] = useState('');
 
     useEffect(() => {
         fetch(ApiUrl)
@@ -22,8 +22,8 @@ export default function AppHeader() {
                     <img src="https://fontmeme.com/permalink/250729/f783700a65bcd965448facb3e6c4617e.png" alt="font-netflix" border="0" />
                 </a>
                 <div className="me-3 mt-3 d-flex gap-2 align-itmens-center">
-                    <input type="text" />
-                    <button className="btn btn-outline" onClick={e => setMovies(e.target.value, API_KEY)}>Search</button>
+                    <input type="text" className="px-2 py-1" />
+                    <button className="btn btn-outline" onClick={e => setMovies(e.target.value)}>Search</button>
                 </div>
             </div>
         </>
